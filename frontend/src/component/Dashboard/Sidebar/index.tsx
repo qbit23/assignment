a72004e727726c95684dd1cdddb37b76089c240c
igaurab <dev@igaurab.com>
2024-01-24 23:35:13 +0545

    Fix: Add bg-color on activate link.

--- frontend/src/component/Dashboard/Sidebar/index.tsx
+++ frontend/src/component/Dashboard/Sidebar/index.tsx
@@ -1,15 +1,19 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { sidebarLinks } from "../../../constants";
 import Icon from "../../common/Icon";
 
 export default function Sidebar() {
+  const { pathname } = useLocation();
+
   return (
     <div className="flex flex-col p-4 h-screen bg-[#f9f9fb] border-r-[1px] w-[15rem]">
       {sidebarLinks.map((link) => (
         <NavLink
           key={link.id}
           to={link.route}
-          className={`flex items-center gap-x-3 h-10 hover:bg-[#F2F2F2] px-3 py-2 rounded-md`}
+          className={`flex items-center gap-x-3 h-10 hover:bg-gray-200 px-3 py-2 rounded-md ${
+            pathname === link.route && "bg-gray-300"
+          }`}
         >
           <Icon name={link.icon} className="text-[24px]" />
           <span>{link.name}</span>
